00ab02b03e84b8329a95ef170bd7b8b5c6288530
victor-sp-01 <55814035+victor-sp-01@users.noreply.github.com>
2023-04-16 18:19:50 -0500

    Delete js directory

--- js/pages/PageNotFound.js
+++
@@ -1,8 +0,0 @@
-import ModalApp from "../lib/ModalApp.js"
-
-const PageNotFound =()=>{
-    const $Element = new ModalApp({})
-    $Element.create({ classID : '#main' })
-}
-
-export default PageNotFound
